66591324f7af12ab06aafba4ea7ecce079c66c0c
Sandro Martínez <virgosmp.mx@gmail.com>
2024-11-18 22:51:55 -0600

    end

---
+++ Basics/Bitwise Operations and Bitmasks/Lucky Numbers (easy)/main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    uint64_t ans =  UINT64_MAX;
+    for(int i = 2; i < 19; i += 2) {
+        for (int mask = 0; mask < 1 << i; mask++) {
+            string s;
+            if (__builtin_popcount(mask) == i/2) {
+                for (int j = 0; j < i; j++) {
+                    if (mask & (1 << j)) {
+                        s.push_back('7');
+                    } else {
+                        s.push_back('4');
+                    }
+                }
+                uint64_t num = stoull(s);
+                if (num >= n)
+                    ans = min(ans, num);
+            }
+        }
+    }
+    cout << ans << endl;
+}
