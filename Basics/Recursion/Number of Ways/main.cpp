351dd5f1e691ef40045455be11d1f3615444d78e
Sandro Martínez <virgosmp.mx@gmail.com>
2024-11-24 00:17:10 -0600

    Update main.cpp

--- Basics/Recursion/Number of Ways/main.cpp
+++ Basics/Recursion/Number of Ways/main.cpp
@@ -1,11 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int ways(int S, int E) {
+    if (S == E) {
+        return 1;
+    }
+    if (S > E) {
+        return 0;
+    }
 
+    return ways(S + 1, E) + ways(S + 2, E) + ways(S + 3, E);
+}
 
 int main() {
-    int s, e;
-    cin >> s >> e;
-    int count = 0;
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    int S, E;
+    cin >> S >> E;
+
+    cout << ways(S, E) << endl;
+
     return 0;
 }
